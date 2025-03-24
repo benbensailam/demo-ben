1d07849630191bf943288e959c5144de9ddf8372
benbensailam <hophamsailam@gmail.com>
2025-03-24 15:43:46 +0700

    test branch

---
+++ demo-ben.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    if(fopen("ben.inp", "r")){
+        freopen("ben.inp", "r", stdin);
+        freopen("ben.out", "w", stdout);
+    }
+    solve();
+}
+
